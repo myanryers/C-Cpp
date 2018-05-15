53d9db4137ad1723396f89f1194087720dcbd68e
Ryan Myers <myanryers@icloud.com>
2018-05-14 23:34:04 -0500

    Adds to main.cpp and ignores "main" generated by main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 // my first C++ program
+// simple user I/O and printing variables
 #include <iostream>
+
+// namespace declaration
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	cout >> "Hello World!"
+	// variables
+	int i, j;
+	cout << "Hello World!" << endl;
+	cout << "Please enter an integer: ";
+	cin >> i;
+	cout << "Please enter another integer: ";
+	cin >> j;
+	cout << "Thank you for entering the following:" << endl;
+	cout << "i = " << i;
+	cout << ", j = " << j << endl;
+	cout << "Bye!" << endl;
+
 	return 0;
 }
